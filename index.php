8ef2312ca835437f373b4cc77a7cfeec6e5b0059
kholilurrohman <odienkzkholil@gmail.com>
2024-11-09 15:55:54 +0700

    Menambahkan tombol numerik dan operator pada antarmuka kalkulator

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
     <h1>Kalkulator Sederhana</h1>
         <input type="text" id="display" class="display" value="" disabled>
         <div class="buttons">
-            
+        <button type="button" class="btn" onclick="appendValue('1')">1</button>
+                <button type="button" class="btn" onclick="appendValue('2')">2</button>
+                <button type="button" class="btn" onclick="appendValue('3')">3</button>
+                <button type="button" class="btn" onclick="appendValue('+')">+</button>
+
         </div>
     </div>
 </body>
