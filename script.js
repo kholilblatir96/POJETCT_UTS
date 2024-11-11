ee538315935154e6a2efb4775e66850174b7e224
kholilurrohman <odienkzkholil@gmail.com>
2024-11-11 13:31:19 +0700

    menambahkan logika AJAX untuk mengirim data ke server PHP dan menangani respons

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function clearDisplay() {
 
 function calculateResult() {
     const expression = document.getElementById('display').value;
+
+    // Use AJAX to send the data to the PHP server and get the result
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '', true); // Send data to the current PHP file
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function ()
